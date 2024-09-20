3e9377d77c62f8e37eb36749add46cb40caa7cac
dylanmondrus <dylan.mondrus@gmail.com>
2024-09-19 21:17:16 -0500

    Add files via upload

--- MyNodeBuilder.java
+++ MyNodeBuilder.java
@@ -1,50 +1,33 @@
 package myclassproject.mystorygraph;
 
-import static myclassproject.questexample.QuestStoryEntities.*;
-
-import java.util.List;
-import com.actions.*;
-import com.sequences.*;
-import com.storygraph.*;
-
-public class QuestNodeBuilder extends NodeBuilder {
-	public QuestNodeBuilder(List<Node> list) {
-		super(list);
-
-
-
-
+import static myclassproject.questexample.QuestStoryEntities.bandit;
+import static myclassproject.questexample.QuestStoryEntities.city
+import static myclassproject.questexample.QuestStoryEntities.cottage;
+import static myclassproject.questexample.QuestStoryEntities.player;
+import static myclassproject.questexample.QuestStoryEntities.sword;
+import static myclassproject.questexample.QuestStoryEntities.town;
 
 import java.util.List;
 
 import com.actions.Draw;
 import com.actions.EnableInput;
 import com.actions.Face;
-import com.actions.HideDialog;
 import com.actions.HideMenu;
+import com.actions.HideDialog;
 import com.actions.SetCameraFocus;
 import com.actions.SetPosition;
 import com.actions.ShowMenu;
-import com.entities.Characters;
-import com.entities.Furniture;
-import com.entities.Item;
-import com.entities.Place;
-import com.enums.BodyTypes;
-import com.enums.Clothing;
-import com.enums.Colors;
-import com.enums.FurnitureTypes;
-import com.enums.HairStyle;
-import com.enums.ItemTypes;
-import com.enums.PlaceTypes;
+import com.actions.utility.ShowDialog;
 import com.sequences.CreateAll;
 import com.sequences.CreateCharacterSequence;
 import com.sequences.DialogSequence;
 import com.storygraph.*;
-import com.sequences.NarrationSequence;
+import com.sequence.NarrationSequence;
 import com.actions.HideNarration;
 
+
+
 import myclassproject.questexample.NodeLabels;
-import myclassproject.questexample.ShowDialogue;
 
 public class MyNodeBuilder extends NodeBuilder {
 	public MyNodeBuilder(List<Node> list) {
@@ -61,7 +44,7 @@ public class MyNodeBuilder extends NodeBuilder {
 	public void rootActions() {
 		//Example:
 		var root = get(NodeLabels.root.toString());
-		root.add(new CreateAll(List.of(city, tavern, forestpath, apple, sword, littorch, bottle, coin, hammer, bag))))
+		root.add(new CreateAll(List.of(city, tavern, forestPath, apple, sword, litTorch, bottle, coin, hammer, bag))))
 		.add(new CreateCharacterSequence(doug))
 		.add(new CreateCharacterSequence(merchantbill))
 		.add(new CreateCharacterSequence(bandit))
@@ -77,39 +60,51 @@ public class MyNodeBuilder extends NodeBuilder {
 		//.add(new Face(bandit, player)).add(new Draw(bandit, sword))
 		.add(new SetCameraFocus(doug))
 		.add(new ShowMenu());
-	
 		
-	@BuilderMethod
-	public void StartGame() {
-		var node = get(NodeLablels.StartGame.toString());
-		node.add(HideMenu().add(new NarrationSequence("This game is an open world game where you can visit multiple locations interact with people,"
-				+ " and accept quests. Your character is Doug Do Good. He has lived all his life as a peasant on a farm,"
-				+ " but has decided to leave his home in search for more adventure."));
+
 	}
-				
+	@BuilderMethod
+	public startGame() {
+		var node = get(NodeLabels.startGame.toString());
+		node.add(new HideMenu().add(new NarrationSequence("In this game you can visit multiple locations,"
+				+ " interact with people, and accept quests. Your character is"
+				+ " Doug Do Good. He has lived all his life as a peasant on a "
+				+ "farm, but has decided to leave his home in search for more"
+				+ " adventure. All his life he has lived as a good man. Your"
+				+ " goal is to remain a good man."));
+	}
+	
 	@BuilderMethod
 	public void atCityActions() {
 		var node = get(NodeLabels.atCity.toString());
-		node.add(new HideNarration()).add(new NarrationSequence("Inside the city square there are many places you can go to interact with objects and people./n"
-				+.add(new EnableInput());
+		node.add(new HideDialog()).add(new HideMenu()).add(new EnableInput())
+		.add(new ShowDialog("Inside the city square there are many places "
+				+ "you can go and visit people to talk to."));
+	}
+	
+	@BuilderMethod
+	public void MerchantBillTalkActions() {
+		var node = get(NodeLabels.MerchantBillTalk.toString());
+		node.add(new DialogSequence(doug, merchantbill, List.of("Greeting, I am Merchant Bill. "
+				+ "I have no goods to sell you because I was vicously beaten and robbed by a bandits."
+				+ " They stole my precious and valuable bag filled with items. I would do anything to"
+				+ " get it back, but I am just an old man who can not confront the bandits. Would you be"
+				+ " so kind to help me. You will be rewarded handsomely If you help me."),
+				List.of("Accept Quest!", "Decline Quest")));
 	}
 		
-	}
-	@BuilderMethod
-	public void merchantbillTalkActions() {
-		var node = get(NodeLabels.merchantbillTalk.toString());
-		node.add(new HideNarration().add(new DialogSequence(player, merchantbill, List.of("Greeting, I am Merchant Bill."
-				+ " I have no goods to sell you because I was vicously beaten and robbed by bandits."
-				+ " They stole by precious and valuable bag filled with items."
-				+ " I would do anything to get it back but I am just an old man who can not confront the bandits please help me."
-				+ " Would you be so kind to help me. You will be rewarded handsomely If you help me."),
-				List.of("Accept Quest", "Decline Quest")));
-	}
+	
 	@BuilderMethod
 	public void acceptActions() {
-		var node = get
-		
+		var node = get()
 	}
 	
 }
+	
+	
+	
+	
+	
+	
+	
 	
--- QuestStoryEntities.java
+++ QuestStoryEntities.java
@@ -1,17 +1,37 @@
 package myclassproject.questexample;
 
+import java.awt.Color;
+
 import com.entities.*;
 import com.enums.*;
 
 public final class QuestStoryEntities {
-	public static final Characters player = new Characters("Player", BodyTypes.D, Clothing.Peasant, HairStyle.Short, Colors.Black, 6);
+	public static final Characters doug = new Characters("Doug", BodyTypes.D, Clothing.Peasant, HairStyle.Short, Colors.Black, 6);
 	public static final Characters bandit = new Characters("Bandit", BodyTypes.F, Clothing.Bandit, HairStyle.Bald, Colors.Black, 4);
-	public static final Place cottage = new Place("Home", PlaceTypes.Cottage);
-	public static final Place town = new Place("Town", PlaceTypes.City);
+	public static final Place city = new Place("City", PlaceTypes.City);
 	public static final Item sword = new Item("Sword", ItemTypes.Sword);
 	public static final Furniture cottageDoor = new Furniture(cottage, FurnitureTypes.Door);
 	public static final Furniture table = new Furniture(cottage, FurnitureTypes.Table);
 	public static final Furniture chair = new Furniture(cottage, FurnitureTypes.Chair);
 	public static final Furniture cityDoor = new Furniture(town, FurnitureTypes.RedHouseDoor);
 	public static final Furniture cityExit = new Furniture(town, FurnitureTypes.NorthEnd);
+	public static final Characters beggar = new Characters("Beggar", BodyTypes.A, Clothing.Peasant, HairStyle.Long, Colors.Brown, 1);
+	public static final Characters drunkard = new Characters("Drunkard", BodyTypes.D, Clothing.Peasant, HairStyle.Short_Full, Colors.Red, 1);
+	public static final Characters merchantbill = new Characters("Merchant Bill", BodyTypes.B, Clothing.Merchant, HairStyle.Spiky, Colors.Gray, 1);
+	public static final Characters bartender = new Characters("Bartender", BodyTypes.B, Clothing.Noble, HairStyle.Short_Full, Colors.Blonde, 1);
+	public static final Place tavern = new Place("Tavern", PlaceTypes.Tavern);
+	public static final Place forestpath = new Place("Forest Path", PlaceTypes.ForestPath);
+	public static final Item apple = new Item("Apple", ItemTypes.Apple);
+	public static final Item bottle = new Item("Beer", ItemTypes.Bottle);
+	public static final Item bag = new Item("Bag", ItemTypes.Bag);
+	public static final Item coin = new Item("Coin", ItemTypes.Coin);
+	public static final Item littorch = new Item("Lit Torch", ItemTypes.LitTorch);
+	public static final Item hammer = new Item("Hammer", ItemTypes.Hammer);
+	
+	
+
+
+
+
+
 }
