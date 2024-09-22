af634651940488974534c70bd70556e268cb6970
jburch33 <jburch5@tulane.edu>
2024-09-22 15:39:43 -0500

    Add files via upload

--- MyNodeLabels.java
+++ MyNodeLabels.java
@@ -2,5 +2,13 @@ package myclassproject.mystorygraph;
 
 //Fill this enum with the labels you chose for the NODES of your story graph.
 public enum MyNodeLabels{
-	root, //other nodes
+	root,
+	StartGame,
+	atCityActions,
+	merchantbillTalkActions,
+	AcceptActions,
+	atForestPathActions,
+	wellActions,
+	BanditSwordActions
+	//other nodes
 }
