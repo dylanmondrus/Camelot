27dfce4647c2247560118922a821581d4396be33
JacksonB-123 <jacksonhardyburch@icloud.com>
2024-10-27 15:23:37 -0500

    Add files via upload

--- MyChoiceLabels.java
+++ MyChoiceLabels.java
@@ -2,29 +2,22 @@ package myclassproject.mystorygraph;
 
 //Fill this enum with the labels you chose for the EDGES of your story graph.
 public enum MyChoiceLabels {
+	StartGameEdges,
 	rootEdges,
-	StartGameEdges, 
-	atCityEdges, gotoBarrell, 
-	gotoMerchantBill, 
-	PickUpApple, 
-	WalkToFountain, 
-	TalkToBeggar, 
-	WalkToTavern, 
-	SitInTavern,
-	WalkToDrunkard,
-	IgnoreDrunkard,
-	TalkToDrunkard,
-	TalkingToDrunkard,WakeUpDrunkard,
-	WakeDrunkard,
-	RobDrunkard,
-	LeaveDrunkard,
-	PersistDrunkard,
-	GoToBar,
-	BuyDrink,
-	LeaveTavern,
-	DrunkardDoor,
-	
-	
+	atCityEdges,
+	gotoBarrell,
+	gotoMerchantBill,
+	PickUpApple,
+	WalkToFountain,
+	TalkToBeggar,
+	WalkToMerchantBill, 
+	TalkToMerchantBill,
+	gotoForestPath,
+	ContinueDrinkingChoice,
+	GoToFireplaceChoice,
+	VisitTavernTableChoice,
+	BurnedToDeathChoice,
+	PickupTorchChoice,
 	
 	
 }
