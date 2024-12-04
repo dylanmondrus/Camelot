a86e18809b9213129e33eda3e6bd4d5a1ae7c096
JacksonB-123 <jacksonhardyburch@icloud.com>
2024-12-04 13:17:03 -0600

    Add files via upload

--- MyNodeBuilder.java
+++ MyNodeBuilder.java
@@ -1,3 +1,4 @@
+
 package myclassproject.mystorygraph;
 
 import static myclassproject.mystorygraph.MyStoryEntities.*;
@@ -121,7 +122,7 @@ public class MyNodeBuilder extends NodeBuilder {
     public void ForestPlantActions() { //Dylan
     	var node = get(MyNodeLabels.ForestPlant.toString());
     	node.add(new HideDialog()).add(new WalkTo(doug, forestplant)).add(new Kneel(doug)).add(new NarrationSequence("You are amazed by the plant and get distracted for a moment "
-    	+ "but you refocus on the mission at hand.")).add(new Wait(3)).add(new HideNarration())
+    	+ "but you refocus on the mission at hand.")).add(new Wait(4)).add(new HideNarration())
     	.add(new WalkTo(doug, well));
     	
     }
@@ -129,7 +130,7 @@ public class MyNodeBuilder extends NodeBuilder {
     public void wellActions() { //Dylan
     	var node = get(MyNodeLabels.well.toString());
     	node.add(new HideNarration()).add(new NarrationSequence("While at the well you discover a sword at the bottom of it."
-    			+ " Pick up the sword, and confront the bandit.")).add(new Wait(3)).add(new HideNarration()).add(new EnableInput()).add(new Pickup(doug, sword))
+    			+ " Pick up the sword, and confront the bandit.")).add(new Wait(5)).add(new HideNarration()).add(new EnableInput()).add(new Pickup(doug, sword))
     	.add(new WalkTo(doug,bandit));
     }
     @BuilderMethod  //
@@ -146,7 +147,7 @@ public class MyNodeBuilder extends NodeBuilder {
     	.add(new Attack(doug, bandit))
     	.add(new Die(bandit))
     	.add(new NarrationSequence("You swiftly murdered the bandit with your sword, and take the bag, but you have broken your morals you are no longer a good man."))
-    	.add(new Wait(3))
+    	.add(new Wait(6))
     	.add(new HideNarration())
     	.add(new Pocket(doug, sword))
         .add(new SetPosition(bag, forestpath, "DirtPile"))
@@ -162,7 +163,7 @@ public class MyNodeBuilder extends NodeBuilder {
     var node = get(MyNodeLabels.Retreat.toString());
     node.add(new HideDialog()).add(new NarrationSequence("As Doug Do Good you have sworn to never harm anybody and you will not fold your morals for money. "
     + "Although you may not have gotten the poor old Merchant Bill's goods you still have your pride and morals."))
-    .add(new Wait(3))
+    .add(new Wait(10))
     .add(new HideNarration())
     .add(new WalkTo(doug, forestexit))
     .add(new EnableInput());
@@ -189,7 +190,7 @@ public class MyNodeBuilder extends NodeBuilder {
     	var node = get(MyNodeLabels.AcceptKing.toString());
     	node.add(new HideDialog())
     	.add(new NarrationSequence("You become king of the Kingdom of Camelot and are remembered as a benevolent and passive ruler. The End."))
-    	.add(new Wait(5))
+    	.add(new Wait(6))
     	.add(new HideNarration())
     	.add(new CreditsSequence("Credits: Dylan Mondrus, Jackson Burch, Joshua Burch. Course: CMPS 1600"));
     	
@@ -209,7 +210,7 @@ public class MyNodeBuilder extends NodeBuilder {
     	node.add(new HideDialog())
     	.add(new NarrationSequence("You are remembered as being too kind to be king."
     	+ " You die a good man and that is all that ever mattered to you."))
-    	.add(new Wait(5))
+    	.add(new Wait(7))
     	.add(new HideNarration())
     	.add(new FadeOut())
     	.add(new CreditsSequence("Thank you for playing our game!"
@@ -228,11 +229,11 @@ public class MyNodeBuilder extends NodeBuilder {
     	var node = get(MyNodeLabels.DenyKing.toString());
     	node.add(new DialogSequence(doug, king, List.of("I'm sorry, but I can not rule. I am afraid"
     	+ " that the power would change me and force me to harm others"), List.of(" ")))
-    	.add(new Wait(3))
+    	.add(new Wait(6))
     	.add(new HideDialog())
     	.add(new NarrationSequence("Although you are not king you are still a good man"
     	+ " and that is all that has ever mattered to you."))
-    	.add(new Wait(3))
+    	.add(new Wait(4))
     	.add(new HideNarration())
     	.add(new FadeOut())
     	.add(new CreditsSequence("Thank you for playing our game!"
@@ -293,7 +294,7 @@ public class MyNodeBuilder extends NodeBuilder {
     	+ " You were cruel to your people and vicious on the battle field. "
     	+ "After you killed the Bandit your morals were compromised and you were blood thirsty."
     	+ " You died doing what you loved-killing in the battle field."))
-    	.add(new Wait(5))
+    	.add(new Wait(9))
     	.add(new HideNarration())
     	.add(new Die(doug))
     	.add(new HideNarration())
@@ -318,7 +319,7 @@ public class MyNodeBuilder extends NodeBuilder {
     	.add(new NarrationSequence("In prison you think of that day and how you killed that man. "
     	+ "It is all that you think about and it haunts you. "
     	+ "You die in prison knowing that you are a bad man."))
-    	.add(new Wait(3))
+    	.add(new Wait(9))
     	.add(new HideNarration())
     	.add(new Die(doug))
     	.add(new Wait(2))
@@ -373,7 +374,7 @@ public class MyNodeBuilder extends NodeBuilder {
         var node = get(MyNodeLabels.FountainYes.toString());
         node.add(new HideDialog()).add(new Give(doug, apple, beggar))
             .add(new NarrationSequence("She is so grateful for your kindness that she reccommends you go to the tavern and have some fun."))
-            .add(new Wait(3)).add(new HideNarration())
+            .add(new Wait(4)).add(new HideNarration())
             .add(new Dance(beggar))
             .add(new WalkTo(doug, redhousedoor));
 
@@ -394,7 +395,7 @@ public class MyNodeBuilder extends NodeBuilder {
            // .add(new FadeOut())
             //.add(new Enter(doug, taverndoor, true))
             node.add(new FadeIn())//.add(new WalkTo(doug, drunkard))
-            .add(new NarrationSequence("At the tavern there are empty chairs to sit in where you can drink. There is also a drunkard who looks like he has had too many drinks.")).add(new Wait(5))
+            .add(new NarrationSequence("At the tavern there are empty chairs to sit in where you can drink. There is also a drunkard who looks like he has had too many drinks.")).add(new Wait(7))
             .add(new HideNarration())
             .add(new DialogSequence(doug, doug, List.of("..."), List.of("Sit", "Talk to Drunkard")));
     }
@@ -407,7 +408,7 @@ public class MyNodeBuilder extends NodeBuilder {
         	.add(new Wait(4)).add(new HideNarration()).add(new Take(doug, bottle,bartender))
             .add(new Drink(doug)).add(new Face(doug, drunkard)).add(new Wait(1))
             .add(new Dance(drunkard))
-            .add(new NarrationSequence("You enjoy a beverage, but this guy at the bar is wasted and looks like he needs help. Do you ignore him and party or do you help him?")).add(new Wait(2))
+            .add(new NarrationSequence("You enjoy a beverage, but this guy at the bar is wasted and looks like he needs help. Do you ignore him and party or do you help him?")).add(new Wait(7))
             .add(new HideNarration())
             .add(new EnableInput());
     }
@@ -428,7 +429,7 @@ public class MyNodeBuilder extends NodeBuilder {
         node.add(new HideDialog()).add(new DisableInput()).add(new Pocket(doug, bottle)).add(new Take(doug, bottle, bartender)).add(new Drink(doug)).add(new Laugh(doug))
             .add(new Face(doug, fireplace))
             .add(new NarrationSequence("You start feeling tipsy and like a moth to a flame the fire catches your attention. Do you go to the fireplace and watch or do you go to the torch that is on the table?"))
-            .add(new Wait(2)).add(new HideNarration()).add(new EnableInput())
+            .add(new Wait(10)).add(new HideNarration()).add(new EnableInput())
             /*.add(new DialogSequence(doug, doug, List.of("..."), List.of("Table", "Fireplace")))*/;
     }
 
@@ -440,7 +441,7 @@ public class MyNodeBuilder extends NodeBuilder {
         	//.add(new WalkTo(doug, taverntable))
         	.add(new DisableInput())
             .add(new NarrationSequence("Something feels sketchy about playing with fire. Last fall your Uncle John Do Bad set his house on fire, but he was kinda a loser. Do you pick up the torch?"))
-            .add(new Wait(3))
+            .add(new Wait(10))
             .add(new HideNarration())
             .add(new EnableInput())
             /*.add(new DialogSequence(doug, doug, List.of("You see a torch flickering enticingly nearby."), List.of("Pick up the torch", "Do not pick up the torch")))*/;
@@ -452,7 +453,7 @@ public class MyNodeBuilder extends NodeBuilder {
         var node = get(MyNodeLabels.PickupTorch.toString());
         node.add(new DisableInput()).add(new Pickup(doug, littorch))
             .add(new NarrationSequence("In an innibriated state you hastily pick up the torch. Ouch you get a splinter and drop the torch. The tavern burns to the ground with you and that weird drunk guy inside."))
-            .add(new Wait(3)).add(new HideNarration())//.add(new Putdown(doug,littorch))
+            .add(new Wait(7)).add(new HideNarration())//.add(new Putdown(doug,littorch))
             .add(new CreateEffect(doug, com.enums.Effects.Spiralflame))
             .add(new EnableEffect(doug, com.enums.Effects.Spiralflame))
             .add(new Die(doug)).add(new EnableInput())
@@ -464,7 +465,7 @@ public class MyNodeBuilder extends NodeBuilder {
     public void BurnedToDeath() {
         // Dylan
         var node = get(MyNodeLabels.BurnedToDeath.toString());
-        node.add(new HideDialog()).add(new DisableInput()).add(new NarrationSequence("You let drinking and risky behavior take priority over being a good man. You and an innocent drunkard died because of your foolish actions.")).add(new Wait(2))
+        node.add(new HideDialog()).add(new DisableInput()).add(new NarrationSequence("You let drinking and risky behavior take priority over being a good man. You and an innocent drunkard died because of your foolish actions.")).add(new Wait(7))
             .add(new HideNarration())
             .add(new FadeOut()).add(new CreditsSequence("Thank you for playing our game!"
                 	+ "Contributors: Dylan Mondrus-Jackson Burch-Joshua Burch"));
@@ -474,7 +475,7 @@ public class MyNodeBuilder extends NodeBuilder {
     public void DontPickupTorch() {
         // Dylan
         var node = get(MyNodeLabels.DontPickupTorch.toString());
-        node.add(new NarrationSequence("OOHH Doug likes the fireplace. You walk over to the fireplace")).add(new Wait(2))
+        node.add(new NarrationSequence("OOHH Doug likes the fireplace. You walk over to the fireplace")).add(new Wait(5))
             .add(new HideNarration())
             .add(new WalkTo(doug, fireplace));
     }
@@ -486,7 +487,7 @@ public class MyNodeBuilder extends NodeBuilder {
         node.add(new WalkTo(doug, fireplace))
             .add(new LookAt(doug, fireplace))
             .add(new NarrationSequence("While walking over to the fireplace you trip and fall into the fire because you had to much to drink. You burn to death."))
-            .add(new Wait(3)).add(new HideNarration()).add(new Kneel(doug)).add(new CreateEffect(doug, com.enums.Effects.Spiralflame))
+            .add(new Wait(6)).add(new HideNarration()).add(new Kneel(doug)).add(new CreateEffect(doug, com.enums.Effects.Spiralflame))
             .add(new EnableEffect(doug, com.enums.Effects.Spiralflame))//add visual effect
             .add(new Die(doug))
             .add(new FadeOut()).add(new CreditsSequence("Thank you for playing our game!"
@@ -509,7 +510,7 @@ public void ApproachDrunkard() { //Joshua Fixed
 @BuilderMethod
 public void ContinueTalking() { //Joshua Fixed
 	var node = get(MyNodeLabels.ContinueTalking.toString());
-	node.add(new DisableInput()).add(new HideDialog()).add(new NarrationSequence("The drunkard is wobbling and slurring his words. The Drunkard is about to passes out.")).add(new Wait(2))
+	node.add(new DisableInput()).add(new HideDialog()).add(new NarrationSequence("The drunkard is wobbling and slurring his words. The Drunkard is about to passes out.")).add(new Wait(6))
         .add(new HideNarration()).add(new EnableInput())
         .add(new DialogSequence(drunkard, doug, List.of("I...I don't need anythiiiii..."),List.of("Wake Drunkard", "Rob Drunkard")))
          // The drunkard passes out, but revive will be used later to wake him.
@@ -521,7 +522,7 @@ public void ContinueTalking() { //Joshua Fixed
 public void RobDrunkard() { //Joshua Fixed
     var node = get(MyNodeLabels.RobDrunkard.toString());
     node.add(new DisableInput()).add(new HideDialog()).add(new Take(doug, bottle, drunkard)).add(new Die(drunkard)).add(new Drink(doug))
-        .add(new NarrationSequence("After taking the drunkards drink he passes out and you drink his. You decide to buy another drink.")).add(new Wait(2)).add(new HideNarration()).add(new Face(doug,bartender))
+        .add(new NarrationSequence("After taking the drunkards drink he passes out and you drink his. You decide to buy another drink.")).add(new Wait(5)).add(new HideNarration()).add(new Face(doug,bartender))
         .add(new WalkTo(doug,bartender))
         .add(new EnableInput());
 }
@@ -531,7 +532,7 @@ public void WakeDrunkard() { //Joshua Fixed
     node.add(new DisableInput()).add(new HideDialog()).add(new Face(doug,drunkard)).add(new Die(drunkard)).add(new Wait(1)).add(new NarrationSequence("The drunkard has passed out. Try and Wake him up"))
         .add(new Wait(3)).add((new HideNarration())).add(new Clap(doug)).add(new Wait(1))
         .add(new Revive(drunkard)) //using revive as a way for drunkard to wake up
-        .add(new NarrationSequence("Your claps have awoken the drunkard but he is angry that you woke him.")).add(new Wait(2))
+        .add(new NarrationSequence("Your claps have awoken the drunkard but he is angry that you woke him.")).add(new Wait(4))
         .add(new HideNarration()).add(new EnableInput())
         .add(new DialogSequence(drunkard, doug, List.of("I...I... I'm not drunk. I'm stone cold sober. You are drunk not me!"),List.of("Persist Drunkard", "Ignore Drunkard")))
         /*.add(new NarrationSequence("You are beginning to feel frustrated with the drunkard. Will you persist in trying to help him?"))
@@ -577,7 +578,7 @@ node.add(new HideDialog())
 .add(new SetNight())
 .add(new Wait(2))
 .add(new NarrationSequence("LAST ONE TO MY HOUSE IS A ROTTEN EGG!"))
-.add(new Wait(2))
+.add(new Wait(3))
 .add(new DisableInput())
 .add(new HideNarration())
   .add(new WalkTo(drunkard, drunkardhousedoor))
@@ -624,4 +625,3 @@ public void Conclusion() {
   + "Contributors: Dylan Mondrus Joshua Burch and Jackson Burch"));
 }
 }
-
